b5adc2acb88ad60889e6cc4b6ed3c2bf3ca437e3
pouterr <poutergutorov@gmail.com>
2023-04-08 21:37:09 +0600

    Implementation of function to compute gcd

--- src/Task10.java
+++ src/Task10.java
@@ -1,12 +1,12 @@
 public class Task10 {
-    public static int gcd(int a, int b){
+    public static int computeGcd(int a, int b){
         //if b is zero then GCD is a
         if (b == 0) {
             return a;
         }
         //compute GCD of b and the remainder of a divided by b
         else {
-            return gcd(b, a % b);
+            return computeGcd(b, a % b);
         }
     }
 }
